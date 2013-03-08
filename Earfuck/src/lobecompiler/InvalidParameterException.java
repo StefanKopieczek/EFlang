d5cc291f6fa2535abc68ff16e3ec3b7eedebd11d
Stefan Kopieczek <stefankopieczek@gmail.com>
2013-03-08 04:30:52 +0000

    Added InvalidParameterException

---
+++ Earfuck/src/lobecompiler/InvalidParameterException.java
@@ -0,0 +1,10 @@
+package lobecompiler;
+
+public class InvalidParameterException extends Exception {
+	public InvalidParameterException() {
+		super();
+	}
+	public InvalidParameterException(String msg) {
+		super(msg);
+	}
+}
