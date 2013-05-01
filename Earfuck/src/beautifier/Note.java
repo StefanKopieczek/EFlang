ec49d798f3774d31c109818f0795a9ba1b7e5964
Stefan Kopieczek <stefankopieczek@gmail.com>
2013-05-01 17:26:06 +0100

    Finished writing basic functionality of 'Note' class.

--- Earfuck/src/beautifier/Note.java
+++ Earfuck/src/beautifier/Note.java
@@ -4,13 +4,15 @@ package beautifier;
  * Represents a single pitch.
  * @author Stefan
  * 
- * TODO: Custom comparison; range checking in constructor, next-note, prev-note
- * and changePitch; possibly 'get notes in range' function.
+ * TODO: toString();
  */
 
-public class Note {
+public class Note implements Comparable<Note> {
     private static final Note minNote = new Note("a2");
-    private static final Note maxNoteString = new Note("b6");
+    private static final Note maxNote = new Note("b6");
+    
+    private static final String[] noteNames = {"c", "c#", "d", "eb", "e", "f", 
+    	                                       "f#", "g", "g#", "a", "bb", "b"};
     
     private int mNoteValue;
     
@@ -26,7 +28,7 @@ public class Note {
         int asciiIndexOfLowercaseC = 99;
         int semitonesInOctave = 13;
         int noteValue = (int)noteString.charAt(0) - asciiIndexOfLowercaseC;        
-        int octave = (int)(noteString.charAt(noteString.length()-1));
+        int octave = Character.getNumericValue(noteString.charAt(noteString.length()-1));
         
         noteValue += noteValue < 0 ? 13 : 0; // Add 8ve if == a or b.     
         noteValue += semitonesInOctave * octave;
@@ -40,39 +42,76 @@ public class Note {
                 default:  throw new InvalidNoteException(noteString);                          
             }
         }
-                
+        
         mNoteValue = noteValue;
+        
+        if (minNote != null && this.compareTo(minNote) < 0 || 
+            maxNote != null && this.compareTo(maxNote) > 0) {
+        	throw new NoteOutOfBoundsException(this);
+        }                        
     }
     
-    public String toString() {
-        // TODO
-        return null;
+    public int compareTo(Note other) {
+    	return mNoteValue - other.getNoteValue();
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+    	boolean result;
+    	if (!(other instanceof Note)) {
+    		result = false;
+    	}
+    	else {
+    		result = mNoteValue == ((Note)other).getNoteValue();
+    	}
+    	return result;
+    }
+    
+    public int getNoteValue() {
+    	return mNoteValue;
+    }
+    
+    public String toString() {    	
+        int mRawNoteValue = mNoteValue % 13;
+        int octaves = mNoteValue / 13;        
+        String result = noteNames[mRawNoteValue] + Integer.toString(octaves);
+        return result;        
     }
     
     public Note getNextNote() {
-        // TODO
-        return null;
+        Note newNote = new Note(this);
+        newNote.changePitch(1);
+        return newNote;
     }
     
     public Note getPrevNote() {
-        // TODO
-        return null;
+    	Note newNote = new Note(this);
+        newNote.changePitch(-1);
+        return newNote;
     }    
     
-    public void changePitch(int delta) {
-        // Need to check note bounds here.
-        mNoteValue += delta;
+    public void changePitch(int delta) {        
+    	if (mNoteValue + delta < minNote.getNoteValue() ||
+    	    mNoteValue + delta > maxNote.getNoteValue()) 
+    	{
+    		throw new NoteOutOfBoundsException(this.toString() + " + " + delta + " semitones");
+    	}
+    	else 
+    	{
+    		mNoteValue += delta;
+    	}
     }       
     
     protected boolean isValidNoteString(String noteString) {
         boolean isValid = true;        
         char noteChar = noteString.charAt(0);
         char octChar = noteString.charAt(noteString.length() - 1);
-        char accidentalChar = noteString.length() == 3 ? noteString.charAt(2) : '\u0000';        
+        char accidentalChar = noteString.length() == 3 ? noteString.charAt(1) : '\u0000';        
         isValid &= (noteString.length() == 2 || noteString.length() == 3);
         isValid &= (noteChar >= 'a' && noteChar <= 'g');
         isValid &= (octChar >= '2' && octChar <= '6');
         isValid &= (accidentalChar =='\u0000' || accidentalChar == 'b' || accidentalChar == '#');
         return isValid;
-    }    
+    }
+       
 }
--- Earfuck/src/beautifier/NoteOutOfBoundsException.java
+++ Earfuck/src/beautifier/NoteOutOfBoundsException.java
@@ -4,5 +4,9 @@ public class NoteOutOfBoundsException extends RuntimeException {
     public NoteOutOfBoundsException(String message) {
         super(message);
     }
+
+	public NoteOutOfBoundsException(Note note) {
+		this(note.toString());
+	}
 }
 
