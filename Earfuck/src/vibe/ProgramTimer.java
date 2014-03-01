cd434b7d281b6b29c9fbf7065d0987a660204bd5
Ryan Norris <rynorris@gmail.com>
2014-03-01 17:50:03 +0000

    Made ProgramTimer work.

--- Earfuck/src/vibe/ProgramTimer.java
+++ Earfuck/src/vibe/ProgramTimer.java
@@ -2,6 +2,7 @@ package vibe;
 
 import javax.swing.SwingUtilities;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  * ProgramTimer - A threaded timer for timing how long a program has been
@@ -11,9 +12,78 @@ import java.awt.event.ActionListener;
  */
 
 public class ProgramTimer { 
-    public ProgramTimer(int msPerTick, ActionListener listener) {}
-    public void start() {}
-    public void stop() {}
-    public void reset() {}
-    public int getTicks() {return 0;}
+    private int mMsPerTick;
+    private int mTicks = 0;
+    private TimerListener mListener;
+    private TimerThread mThread;
+
+    public ProgramTimer(int msPerTick, TimerListener listener) {
+        mMsPerTick = msPerTick;
+        mListener = listener;
+    }
+
+    public void start() {
+        if (mThread == null) {
+            mThread = new TimerThread(this, mMsPerTick);
+        }
+        if (!mThread.isRunning()) {
+            mThread.start();
+        }
+    }
+
+    public void stop() {
+        if (mThread != null) {
+            mThread.stopRunning();
+        }
+    }
+
+    public void reset() {
+        stop();
+        mTicks = 0;
+    }
+
+    private void tick() {
+        mTicks++;
+        mListener.onTick();
+    }
+
+    public int getTicks() {
+        return mTicks;
+    }
+
+    public static interface TimerListener {
+        public void onTick();
+    }
+
+    private static class TimerThread extends Thread {
+        private int mMsPerTick;
+        private ProgramTimer mTimer;
+        private boolean mRunning = false;
+        
+        public TimerThread(ProgramTimer timer, int msPerTick) {
+            mTimer = timer;
+            mMsPerTick = msPerTick;
+        }
+
+        public void run() {
+            mRunning = true;
+            while (mRunning) {
+                try {
+                    Thread.sleep(mMsPerTick);
+                }
+                catch (InterruptedException e) {
+                    //Do nothing
+                }
+                mTimer.tick();
+            }
+        }
+        
+        public void stopRunning() {
+            mRunning = false;
+        }
+
+        public boolean isRunning() {
+            return mRunning;
+        }
+    }
 }
