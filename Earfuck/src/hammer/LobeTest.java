b77e3c7ca5f77779918473f61274509f45ff0562
Ryan Norris <rynorris@gmail.com>
2014-03-02 22:07:52 +0000

    Added HAMMER test framework.

---
+++ Earfuck/src/hammer/LobeTest.java
@@ -0,0 +1,36 @@
+package hammer;
+
+import lobecompiler.LOBECompiler;
+import lobecompiler.LobeCompilationException;
+import earcompiler.EARCompiler;
+import earcompiler.EARException;
+
+/**
+ * A test on LOBE code.
+ * Simply compiles to ef code, and runs as a usual HammerTest.
+ * @author rynor_000
+ *
+ */
+public class LobeTest extends HammerTest {
+
+	public LobeTest(String name, String code) {
+		super(name, code);
+		LOBECompiler lobeCompiler = new LOBECompiler();
+		EARCompiler earCompiler = new EARCompiler();
+
+		try {
+			efCode = lobeCompiler.compile(efCode);
+		} catch (LobeCompilationException e) {
+			System.out.println("Failed to compile LOBE code.");
+			setupFailed = true;
+		}
+		
+		try {
+			efCode = earCompiler.compile(efCode);
+		} catch (EARException e) {
+			System.out.println("Failed to compile EAR code.");
+			setupFailed = true;
+		}
+	}
+
+}
