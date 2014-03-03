8ee9d398725101529b6cdd8d834d5ff4779816c2
Ryan Norris <rynorris@gmail.com>
2014-03-03 19:58:22 +0000

    Added IO, MOV and COPY tests for EAR. Split tests into 3 suites for Ef, EAR and LOBE.

--- Earfuck/src/hammer/Main.java
+++ Earfuck/src/hammer/Main.java
@@ -14,6 +14,22 @@ public class Main {
 		loopTest.addInputTask(4);
 		loopTest.addOutputTask(8);
 		
+		EarTest earIoTest = new EarTest("EAR IO Test",
+				"IN 2\n OUT 2");
+		earIoTest.addInputTask(42);
+		earIoTest.addOutputTask(42);
+		
+		EarTest earMovTest = new EarTest("EAR MOV Test",
+				"MOV 7 2\n OUT 2\n MOV @2 4\n OUT 2\n OUT 4");
+		earMovTest.addOutputTask(7);
+		earMovTest.addOutputTask(0);
+		earMovTest.addOutputTask(7);
+		
+		EarTest earCopyTest = new EarTest("EAR COPY Test",
+				"MOV 7 2\n COPY @2 4 3\n OUT 2\n OUT 4");
+		earCopyTest.addOutputTask(7);
+		earCopyTest.addOutputTask(7);
+		
 		EarTest earAddTest = new EarTest("EAR ADD Test",
 				"MOV 2 0\n MOV 3 1\n ADD @0 1\n OUT 1\n " +
 				"ADD 3 1\n OUT 1");
@@ -27,24 +43,30 @@ public class Main {
 		earSubTest.addOutputTask(1);
 		
 		EarTest earMulTest = new EarTest("EAR MUL Test",
-				"MOV 5 0\n MOV 3 1\n MUL 5 3 2 3\n OUT 2\n " +
-				"MUL @0 3 2 3\n OUT 2\n MUL 5 @1 2 3\n OUT 2\n"+
-                                "MUL @0 @1 2 3\n OUT 2");
+				"MUL 5 3 2 3\n OUT 2\n " +
+				"MOV 5 0\n" +
+				"MUL @0 3 2 3\n OUT 2\n " +
+				"MOV 3 1\n " +
+				"MUL 5 @1 2 3\n OUT 2\n"+
+				"MOV 5 0\n MOV 3 1\n " +
+                "MUL @0 @1 2 3\n OUT 2");
 		earMulTest.addOutputTask(15);
 		earMulTest.addOutputTask(15);
 		earMulTest.addOutputTask(15);
 		earMulTest.addOutputTask(15);
 
-                EarTest earDivTest = new EarTest("EAR DIV Test",
-                                "MOV 7 0\n MOV 3 1\n" +
-                                "DIV 7 3 2 3 4 5 6 7 8\n OUT 2\n" +
-                                "DIV @0 3 2 3 4 5 6 7 8\n OUT 2\n" +
-                                "DIV 7 @1 2 3 4 5 6 7 8\n OUT 2\n" +
-                                "DIV @0 @1 2 3 4 5 6 7 8\n OUT 2\n");
-                earDivTest.addOutputTask(2);
-                earDivTest.addOutputTask(2);
-                earDivTest.addOutputTask(2);
-                earDivTest.addOutputTask(2);
+        EarTest earDivTest = new EarTest("EAR DIV Test",
+	            "DIV 7 3 2 3 4 5 6 7 8\n OUT 2\n" +
+	            "MOV 7 0\n" +
+	            "DIV @0 3 2 3 4 5 6 7 8\n OUT 2\n" +
+	            "MOV 3 1\n" +
+	            "DIV 7 @1 2 3 4 5 6 7 8\n OUT 2\n" +
+	            "MOV 7 0\n MOV 3 1\n" +
+	            "DIV @0 @1 2 3 4 5 6 7 8\n OUT 2\n");
+        earDivTest.addOutputTask(2);
+        earDivTest.addOutputTask(2);
+        earDivTest.addOutputTask(2);
+        earDivTest.addOutputTask(2);
 		
 		LobeTest lobeTest = new LobeTest("LOBE Addition Test",
 				"x = 3\ny = 4\nprint x + y\n" +
@@ -54,16 +76,26 @@ public class Main {
 		lobeTest.addOutputTask(6);
 		lobeTest.addOutputTask(7);
 		
-		HammerSuite suite = new HammerSuite("Test Suite");
-		suite.addTest(ioTest);
-		suite.addTest(loopTest);
-		suite.addTest(earAddTest);
-		suite.addTest(earSubTest);
-		suite.addTest(earMulTest);
-		suite.addTest(earDivTest);
-		suite.addTest(lobeTest);
-
-                earDivTest.run();
-		suite.run();
+		HammerSuite efSuite = new HammerSuite("Ef Test Suite");
+		efSuite.addTest(ioTest);
+		efSuite.addTest(loopTest);
+		
+		HammerSuite earSuite = new HammerSuite("EAR Test Suite");
+		earSuite.addTest(earIoTest);
+		earSuite.addTest(earMovTest);
+		earSuite.addTest(earCopyTest);
+		earSuite.addTest(earAddTest);
+		earSuite.addTest(earSubTest);
+		earSuite.addTest(earMulTest);
+		earSuite.addTest(earDivTest);
+		
+		HammerSuite lobeSuite = new HammerSuite("LOBE Test Suite");
+		lobeSuite.addTest(lobeTest);
+		
+		
+		efSuite.run();
+		earCopyTest.run();
+		earSuite.run();
+		lobeSuite.run();
 	}
 }
