694c409944d801ada6216b20f3756c617f93d08a
Ryan Norris <rnorris@palantir.com>
2017-10-19 14:47:26 +0800

    Add some HAMMER based tests to ef-core

---
+++ ef-core/src/test/java/eflang/core/ParserTest.java
@@ -0,0 +1,38 @@
+package eflang.core;
+
+import eflang.hammer.HammerTest;
+import org.junit.jupiter.api.Test;
+
+class ParserTest {
+
+    @Test
+    void testInputOutput() {
+        new HammerTest("Input and Output", "e4 d4 r c4 d4 r")
+                .giveInput(12)
+                .expectOutput(12)
+                .run();
+    }
+
+    @Test
+    void testIncrement() {
+        new HammerTest("Increment a cell once", "c4 d4 d4 r")
+                .expectOutput(1)
+                .run();
+    }
+
+    @Test
+    void testIncrementThenDecrement() {
+        new HammerTest("Increment, then decrement a cell", "c4 d4 d4 d4 r e4 d4 d4 c4 d4 r")
+                .expectOutput(2)
+                .expectOutput(1)
+                .run();
+    }
+
+    @Test
+    void testLoop() {
+        new HammerTest("Loop to multiply input by 3", "c4 d4 c4 r ( d4 d4 d4 d4 c4 c4 ) d4 r")
+                .giveInput(4)
+                .expectOutput(12)
+                .run();
+    }
+}
