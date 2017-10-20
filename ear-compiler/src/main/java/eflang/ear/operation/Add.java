f2e268bc82b9fbf509cbe148e117e659e9332f3a
Ryan Norris <rnorris@palantir.com>
2017-10-20 16:22:54 +0800

    Replace ADD with intermediate representation

---
+++ ear-compiler/src/main/java/eflang/ear/operation/Add.java
@@ -0,0 +1,55 @@
+package eflang.ear.operation;
+
+import eflang.ear.Argument;
+import eflang.ear.Instruction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Add implements Operation {
+    @Override
+    public List<Instruction> compile(List<Argument> args) {
+        assert args.size() > 1;
+
+        List<Integer> outputCells = args.subList(1, args.size()).stream()
+                .map(Argument::getValue)
+                .collect(Collectors.toList());
+
+        List<Instruction> instructions = new ArrayList<>();
+
+        switch (args.get(0).getType()) {
+            case CONSTANT:
+                int value = args.get(0).getValue();
+                for (int cell : outputCells) {
+                    instructions.add(Instruction.goTo(cell));
+                    for (int i = 0; i < value; i++) {
+                        instructions.add(Instruction.increment());
+                    }
+                }
+                break;
+
+            case CELL:
+                int inputCell = args.get(0).getValue();
+                instructions.add(Instruction.goTo(inputCell));
+
+                // Optimization to ensure we don't flip to happy again at the end of the loop, just to have to reset
+                // to sad again inside the loop.
+                instructions.add(Instruction.ensureSad());
+                instructions.add(Instruction.startLoop());
+                instructions.add(Instruction.decrement());
+
+                instructions.addAll(outputCells.stream().flatMap(cell -> Stream.of(
+                        Instruction.goTo(cell),
+                        Instruction.increment()
+                        )
+                ).collect(Collectors.toList()));
+
+                instructions.add(Instruction.endLoop());
+                break;
+        }
+
+        return instructions;
+    }
+}
