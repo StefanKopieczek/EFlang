e0daef2fd36bc245bf1cef6e53737136af1a7650
Ryan Norris <rnorris@palantir.com>
2017-10-21 21:52:17 +0800

    Do the same for EARCompiler

--- ear-core/src/test/java/eflang/ear/EarCompilerHammerTest.java
+++ ear-core/src/test/java/eflang/ear/EarCompilerHammerTest.java
@@ -1,23 +1,50 @@
 package eflang.ear;
 
-import eflang.hammer.HammerLoader;
-import eflang.hammer.HammerRunner;
+import com.google.common.collect.ImmutableList;
+import eflang.ear.composer.Composer;
+import eflang.ear.composer.GeometricComposer;
+import eflang.ear.composer.OnlyRunsComposer;
+import eflang.ear.composer.SadisticComposer;
+import eflang.ear.core.Scale;
+import eflang.ear.core.Scales;
+import eflang.hammer.*;
+import org.junit.jupiter.api.DynamicNode;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 import java.io.File;
+import java.util.List;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class EarCompilerHammerTest {
 
     @TestFactory
-    Stream<DynamicTest> hammerTests() {
-        HammerLoader loader = new HammerLoader();
-        HammerRunner runner = new HammerRunner();
-        return loader.loadTestsFromDirectory(new File("../tests/ear")).stream()
-                .map(hammerTest -> dynamicTest(hammerTest.getName(), () -> runner.run(hammerTest)));
+    Stream<DynamicNode> hammerTests() {
+         HammerLoader loader = new HammerLoader();
+        List<Composer> composers = ImmutableList.of(
+                new OnlyRunsComposer(Scales.CMajor),
+                new GeometricComposer(Scales.BluesMinor),
+                new SadisticComposer(Scales.CMajorPentatonic),
+                new GeometricComposer(new Scale("c4", "e4", "g4"))
+        );
+
+        List<HammerTest> tests = loader.loadTestsFromDirectory(new File("../tests/ear"));
+        return composers.stream()
+                .map(composer -> dynamicContainer(
+                        composer.toString(),
+                        buildTests(composer, tests)
+                ));
     }
 
+    private Stream<DynamicTest> buildTests(Composer composer, List<HammerTest> tests) {
+        HammerRunner runner = runnerForComposer(composer);
+        return tests.stream().map(test -> dynamicTest(test.getName(), () -> runner.run(test)));
+    }
+
+    private HammerRunner runnerForComposer(Composer composer) {
+        return new HammerRunner().withCodeConverter(TestType.EAR, new EarCodeConverter(composer));
+    }
 }
