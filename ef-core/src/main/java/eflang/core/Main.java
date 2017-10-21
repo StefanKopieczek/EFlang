42252d434c8cd79207c52d22c73ccab888d54ef1
Ryan Norris <rnorris@palantir.com>
2017-10-21 15:11:52 +0800

    Make ef-core Main play a file by default

--- ef-core/src/main/java/eflang/core/Main.java
+++ ef-core/src/main/java/eflang/core/Main.java
@@ -1,15 +1,24 @@
 package eflang.core;
 
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 public class Main {
     public static void main(String[] args) {
-        String efCode = args[0];
+        String filename = args[0];
+        RandomAccessFile file;
+        try {
+            file = new RandomAccessFile(filename, "r");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
         System.out.print("Loading new earfuck parser...");
         Parser efParser = new Parser(new NullPerformer());
         System.out.println("Done!");
 
         System.out.print("Handing music to performer...");
-        efParser.giveMusic(new StringMusicSource(efCode));
+        efParser.giveMusic(new FileMusicSource(file));
         System.out.println("Done!");
 
         System.out.println("Performing...");
