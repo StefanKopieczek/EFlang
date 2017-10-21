d9a0659559a7f40f0358a55264b7df0c9a070c00
Ryan Norris <rnorris@palantir.com>
2017-10-21 21:55:13 +0800

    Remove dead code from ef tests

--- ef-core/src/test/java/eflang/core/EfHammerTest.java
+++ ef-core/src/test/java/eflang/core/EfHammerTest.java
@@ -1,8 +1,5 @@
 package eflang.core;
 
-import eflang.ear.composer.Composer;
-import eflang.ear.composer.GeometricComposer;
-import eflang.ear.core.Scales;
 import eflang.hammer.HammerLoader;
 import eflang.hammer.HammerRunner;
 import org.junit.jupiter.api.DynamicTest;
@@ -19,7 +16,6 @@ class EfHammerTest {
     Stream<DynamicTest> hammerTests() {
         HammerLoader loader = new HammerLoader();
         HammerRunner runner = new HammerRunner();
-        Composer composer = new GeometricComposer(Scales.BluesMinor);
         return loader.loadTestsFromDirectory(new File("../tests/ef")).stream()
                 .map(hammerTest -> dynamicTest(hammerTest.getName(), () -> runner.run(hammerTest)));
     }
